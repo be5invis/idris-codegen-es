0cbe56ebea05dcbcdb6ed3233a0d30d8c9dcb975
be5invis <belleve@typeof.net>
2017-04-25 15:37:45 +0800

    specialize Prelude.List

--- src/IRTS/CodegenJs.hs
+++ src/IRTS/CodegenJs.hs
@@ -402,18 +402,32 @@ type SProj = JsAST -> Int -> JsAST
 
 constructorOptimizeDB :: Map.Map Name (SCtor, STest, SProj)
 constructorOptimizeDB = Map.fromList [
-      item "Prelude.Bool"  "True"    (\_ -> JsBool True)  id          cantProj
-    , item "Prelude.Bool"  "False"   (\_ -> JsBool False) falseTest   cantProj
-    , item "Prelude.Maybe" "Just"    (\[x] -> x)          notNoneTest justProj
-    , item "Prelude.Maybe" "Nothing" (\[] -> JsUndefined) noneTest    cantProj
+      item "Prelude.Bool"  "True"    (const $ JsBool True)  id          cantProj
+    , item "Prelude.Bool"  "False"   (const $ JsBool False) falseTest   cantProj
+
+    , item "Prelude.List"  "::"      cons                   fillList    uncons
+    , item "Prelude.List"  "Nil"     nil                    emptyList   cantProj
+
+    , item "Prelude.Maybe" "Just"    (\[x] -> x)            notNoneTest justProj
+    , item "Prelude.Maybe" "Nothing" (const $ JsUndefined)  noneTest    cantProj
     ]
   where
+    -- constructors
+    nil = const $ JsArray []
+    cons [h, t] = JsMethod (JsArray [h]) "concat" [t]
+
+    -- tests
+    falseTest e = JsUniOp (T.pack "!") e
+    emptyList e = JsBinOp "===" (JsProp e "length") (JsInt 0)
+    fillList e = JsBinOp ">" (JsProp e "length") (JsInt 0)
     noneTest e = JsBinOp "===" e JsUndefined
     notNoneTest e = JsBinOp "!==" e JsUndefined
-    falseTest e = JsUniOp (T.pack "!") e
-    cantProj x j = error $ "This type should be projected"
-   
+
+    -- projections
     justProj x n = x
+    uncons x 1 = JsArrayProj (JsInt 0) x
+    uncons x 2 = JsMethod x "slice" [JsInt 1]
+    cantProj x j = error $ "This type should be projected"
 
     item :: String -> String -> SCtor -> STest -> SProj -> (Name, (SCtor, STest, SProj))
     item "" n ctor test match = (sUN n, (ctor, test, match))
